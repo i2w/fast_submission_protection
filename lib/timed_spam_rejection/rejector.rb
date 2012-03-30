d604a2005a9f4506dac3a9c7ca8084d3d334dbd9
Ian White <ian.w.white@gmail.com>
2012-03-30 11:09:02 +0100

    Adds Rejector object

--- lib/timed_spam_rejection/rejector.rb
+++ lib/timed_spam_rejection/rejector.rb
@@ -1,4 +1,29 @@
 module TimedSpamRejection
+  Delay = 5
+  
+  class Error < RuntimeError; end
+  class NotStartedError < Error; end
+  class TooFastError < Error; end
+  
   class Rejector
+    attr_reader :started, :delay
+    
+    def initialize delay = nil, timer = nil
+      @delay = delay || Delay
+      @timer = timer || DateTime
+    end
+    
+    def start
+      @started = @timer.now
+    end
+    
+    def finish
+      if started
+        earliest_finish_time = started + delay
+        @timer.now > earliest_finish_time or raise TooFastError
+      else
+        raise NotStartedError
+      end
+    end
   end
 end
--- spec/timed_spam_rejection/rejector_spec.rb
+++ spec/timed_spam_rejection/rejector_spec.rb
@@ -1,5 +1,47 @@
 require 'spec_helper'
 
 describe TimedSpamRejection::Rejector do
-  
+  describe '.new <delay>, <timer>' do
+    subject { rejector }
+    
+    let(:rejector)    { described_class.new delay, timer }
+    let(:delay)       { 20 }
+    let(:timer)       { double now: started_at }
+    let(:started_at)  { DateTime.parse('2012-01-01 12:00:00') }
+    
+    it { should be_a TimedSpamRejection::Rejector }
+    
+    describe '#start' do
+      subject { rejector.start }
+      
+      it 'should start the rejector' do
+        subject
+        rejector.started.should == started_at
+      end
+    end
+    
+    describe '#finish' do
+      subject { rejector.finish }
+      
+      context 'when not started' do
+        it { expect{ subject }.to raise_error TimedSpamRejection::NotStartedError }
+      end
+      
+      context 'when started' do
+        before do rejector.start end
+        
+        context 'and not enough time passes' do
+          before do timer.stub(:now).and_return(started_at + delay) end
+
+          it { expect{ subject }.to raise_error(TimedSpamRejection::TooFastError) }
+        end
+        
+        context 'and enough time passes' do
+          before do timer.stub(:now).and_return(started_at + delay + 1) end
+            
+          it { expect{ subject }.to_not raise_error }
+        end
+      end
+    end
+  end
 end
